faa6c27c1539bb24b9d8fc0cad686cc62a971bfd
EnekoHernando <eneko.hernando@opendeusto.es>
2022-12-07 21:48:33 +0100

    DATA BASE INTIALIZATION STARTED

--- Strava_ServerV2/src/server/MP.java
+++ Strava_ServerV2/src/server/MP.java
@@ -35,7 +35,7 @@ public class MP {
 		}
 		Challenge g1 = new Challenge("Here goes the name", new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis()+86400L), 200, 200, Sport.CYCLING);
 		ChallengeDAO.getInstance().save(g1);
-		User u1 = new User(0, "eneko","1234", new Date(System.currentTimeMillis()), 100, 175,120,60);
+		User u1 = new User(0, "eneko",org.apache.commons.codec.digest.DigestUtils.sha1Hex("1234"), new Date(System.currentTimeMillis()), 100, 175,120,60);
 		u1.getChallengeAL().add(g1);
 		u1.getTraininSL().add(new TrainingSession(u1, "Run until u can breath", Sport.RUNNING, 10, 
 				new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis()+86400L), 10));
